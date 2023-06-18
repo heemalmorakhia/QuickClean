b68982a53f33b021f1b130ac07ac348f2a6b35ae
Tasneem Haque <tasneemhaque@hotmail.com>
2023-06-18 14:07:31 -0300

    Updated folder structured, installed react-dom and implemented routing

---
+++ cleaning-service-website/src/components/footer.js
@@ -0,0 +1,70 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer>
+            <div>
+                <div className="WrostDesignIHaveSeenInMyLife" style={{ marginBottom: "30px" }}></div>
+                <div class="container">
+                    <div class="row">
+                        <div className="col-sm mb-4">
+                            <div className="row">
+                                <img src="" alt="company logo" />
+                            </div>
+                            <div className="row">
+                                <p>Lorem isioga asfi sit aff, sdgsdgherhhhsdh</p>
+                            </div>
+                        </div>
+                        <div className="col-sm mb-5">
+                            <h4>Services</h4>
+                            <div className="footerList ">
+                                <ul className="list-group list-group-flush">
+                                    <li className="list-group-item">Home</li>
+                                    <li className="list-group-item">Office</li>
+                                    <li className="list-group-item">Kito</li>
+                                </ul>
+                            </div>
+
+                        </div>
+                        <div className="col-sm mb-5">
+                            <h4>Blogs</h4>
+                            <div className="footerList">
+                                <ul className="list-group list-group-flush">
+                                    <li className="list-group-item">Recommendation</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="col-sm mb-5">
+                            <h4>Resources</h4>
+                            <div className="footerList">
+                                <ul className="list-group list-group-flush">
+                                    <li className="list-group-item">Help Center</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="col-sm">
+                            <h4>Company</h4>
+                            <div className="footerList ">
+                                <ul className="list-group list-group-flush">
+                                    <li className="list-group-item">About Us</li>
+                                    <li className="list-group-item">Testimonias</li>
+                                    <li className="list-group-item">Terns of Services</li>
+                                    <li className="list-group-item">Privacy Policy</li>
+                                    <li className="list-group-item">Cookie Policy</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="pt-5">
+                        <h6>&copy; - lorem lorem lorem lorem</h6>
+                    </div>
+                </div>
+            </div>
+        </footer>
+
+    );
+
+}
+
+export default Footer;
