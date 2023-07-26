a00d72265143da21588f01019a4a3fbf5f463126
hm518884@dal.ca <hm518884@dal.ca>
2023-07-26 20:26:40 -0300

    proxy change

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,14 +1,11 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
-    app.use(
-        '/graphql',
-        createProxyMiddleware({
-            target: 'http://localhost:3001',
-            changeOrigin: true,
-        })
-    );
-
-    
-
+  app.use(
+    "/graphql",
+    createProxyMiddleware({
+      target: "https://4177-grp25-a3.netlify.app",
+      changeOrigin: true,
+    })
+  );
 };
